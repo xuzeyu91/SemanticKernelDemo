9f85f17650591e8b2cb101b2e3ac44ec1e4f92cf
zyxucp <286513187@qq.com>
2023-11-21 21:38:00 +0800

    add textchunk

---
+++ src/Xzy.SK.Api/Controllers/MsgContextController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.TemplateEngine.Basic;
+using Microsoft.SemanticKernel.Orchestration;
+using Newtonsoft.Json;
+using Microsoft.SemanticKernel.Plugins.Core;
+
+namespace Xzy.SK.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MsgContextController : ControllerBase
+    {
+        private readonly IKernel _kernel;
+
+        public MsgContextController(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        /// <summary>
+        /// 聊天上下文
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Test()
+        {
+            string history = @"User:你好\r\n ChatBot:你好！我是AI助手，有什么需要帮忙的吗？";
+            string input = "我刚才说了什么";
+
+            string FunctionDefinition = @$"
+{history}
+User: {input}";
+
+            var promptRenderer = new BasicPromptTemplateEngine();
+            var renderedPrompt = await promptRenderer.RenderAsync(FunctionDefinition, _kernel.CreateNewContext());
+
+            var test = _kernel.CreateSemanticFunction(FunctionDefinition, requestSettings: new OpenAIRequestSettings() { MaxTokens = 100 });
+            var contextVariables = new ContextVariables
+            {
+                ["$history"] = history,
+                ["$input"] = input
+            };
+            var result = await _kernel.RunAsync(contextVariables, test);
+
+            return Ok(result.GetValue<string>());
+        }
+    }
+}
---
+++ src/Xzy.SK.Api/Controllers/TextChunkController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Plugins.Core;
+using Microsoft.SemanticKernel.Text;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+
+namespace Xzy.SK.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TextChunkController : ControllerBase
+    {
+        private readonly IKernel _kernel;
+
+        public TextChunkController(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        /// <summary>
+        /// 文本分块
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Test()
+        {
+            string msg = System.IO.File.ReadAllText("诛仙.txt");
+
+
+            ///将纯文本拆分成行。
+            ///<param name=“text”>要拆分的文本</param>
+            ///<param name=“maxTokensPerLine”>每行的最大令牌数</param>
+            ///<param name=“tokenCounter”>对字符串中的令牌进行计数的函数。如果未提供，将使用默认计数器</param>
+            ///＜return＞行列表</return>
+            var lines = TextChunker.SplitPlainTextLines(msg, 40);
+
+            ///将纯文本拆分为段落。
+            ///<param name=“lines”>文本行</param>
+            ///<param name=“maxTokensPerParagraph”>每个段落的最大令牌数</param>
+            ///<param name=“overlapTokens”>段落之间重叠的令牌数</param>
+            ///<param name=“chunkHeader”>要在每个单独的区块前加上前缀的文本</param>
+            ///<param name=“tokenCounter”>对字符串中的令牌进行计数的函数。如果未提供，将使用默认计数器</param>
+            ///＜return＞段落列表</return>
+            var paragraphs = TextChunker.SplitPlainTextParagraphs(lines, 4000);
+
+            return Ok();
+        }
+    }
+}
